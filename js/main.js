73f5570a7c73bcac2b10305a490efbe8dc73023a
Elaine Looney <imalooney@gmail.com>
2013-12-11 16:47:51 -0600

    add riot notation to add items to store

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-alert('hi');
+var itemTemplate = $('#templates .item').html();
+
+var newItemHtml = $.render(itemTemplate, { name: 'Dewberry', price: 0.15 });
+$('#store').append(newItemHtml);
+
+var items = [
+  { name: 'PLUM', price: 3 },
+  { name: 'CELERY', price: 0.5 },
+  { name: 'SQUASH', price: 2.5 }
+];
+
+
+for (i=0; i<items.length; i++) {
+  var item= items[i];
+  var newItemHtml = $.render(itemTemplate,{ name: item.name, price: item.price });
+  $('#store').append(newItemHtml);
+};
